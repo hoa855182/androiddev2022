5feb99b18277d1ec9774b736f8e4ec62a1db91d8
hoa855182 <hoa855182@gmail.com>
2021-10-04 16:52:07 +0700

    update lab1,2,3,4

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,16 +3,54 @@ package vn.edu.usth.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class WeatherActivity extends AppCompatActivity {
+    private static final String call ="WeatherActivity";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
+        setContentView(R.layout.fragment_forecast);
+        Log.i(call,"onCreate");
     }
 
-    protected void onStart(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(call,"onStart");
+
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(call,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(call,"onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(call,"onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(call,"onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(call,"onRestart");
+    }
+
 }
